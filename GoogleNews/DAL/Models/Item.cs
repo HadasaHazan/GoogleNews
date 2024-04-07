82bee564b759e1a92302c172444f22b2bc97f3a9
HadasaHazan <150019011+HadasaHazan@users.noreply.github.com>
2024-04-07 18:13:38 +0300

    add files

--- GoogleNews/DAL/Models/Item.cs
+++ GoogleNews/DAL/Models/Item.cs
@@ -12,6 +12,6 @@ namespace DAL.Models
         public string title { get; set; }
         public string body { get; set; }
         public string link { get; set; }
-        public string date { get; set; }
+        public string date { get; set; } 
     }
 }
